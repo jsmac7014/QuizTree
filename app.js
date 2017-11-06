2b3ec9272ab868862b7a62098cdcdfac5d41c3be
Shin-JaeHeon <heoni0504@gmail.com>
2017-11-06 15:13:05 +0900

    Deafult

---
+++ app.js
@@ -0,0 +1,39 @@
+const Express = require('express');
+const Mysql = require('mysql');
+const Path = require('path');
+const Crypto = require('crypto');
+const Logger = require('morgan');
+const Striptags = require('striptags');
+const CookieParser = require('cookie-parser');
+const BodyParser = require('body-parser');
+const Useragent = require('express-useragent');
+const Passport = require('passport');
+const app = Express();
+const isContains = (it, found) => {
+	return Object.keys(it).indexOf(found) > -1 ? true : false;
+};
+const URL = route => {
+	return "https://localhost" + route;
+};
+const query = (url, querys) =>{
+	querys.filter
+}
+app.set('views', Path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.enable('view cache');
+app.use(Express.static('public'));
+app.use(Logger('dev'));
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({
+	extended: false
+}));
+app.use(CookieParser());
+app.use((req, res, next) => {
+	if (isContains(req.query, 'msg')) {
+		if (req.query['msg'] == "error") req.usermsg = "오류가 발생하였습니다.";
+	}
+	next();
+});
+app.get('/',(req,res)=>{
+
+});
